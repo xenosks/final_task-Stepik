ac31cb962a8df88d55fc1afa8846fc10acc1860f
Ksenia <xenosks@gmail.com>
2023-03-04 11:20:11 +0500

    Тесты открытия по ссылке

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,6 @@
-
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-
     def open(self):
-        self.browser.get(self.link)
+        self.browser.get(self.url)
