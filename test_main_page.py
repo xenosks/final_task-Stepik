686ded892eecd4695343e1172055cc7bd6defc6a
Ksenia <xenosks@gmail.com>
2023-03-04 11:44:34 +0500

    Изменение ссылки и селекторов

--- test_main_page.py
+++ test_main_page.py
@@ -2,7 +2,7 @@ from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    url = "http://selenium1py.pythonanywhere.com/"
+    url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
     page = MainPage(browser, url)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
