e4a10eadef1e99782e4c9c5be63a32a677c8a7bf
Ksenia <xenosks@gmail.com>
2023-03-05 11:33:21 +0500

    Добавление parametrize и fail

--- test_product_page.py
+++ test_product_page.py
@@ -1,17 +1,16 @@
 from .pages.product_page import ProductPage
-import time
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    page = ProductPage(browser, url)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+@pytest.mark.parametrize('num', [*range(1,7), pytest.param(7, marks=pytest.mark.xfail), *range(8,10)])
+def test_guest_can_add_product_to_basket(browser, num):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}'
+    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.is_correct_product()
     page.is_price_correct()
-
-
 
 #pytest -s test_product_page.py
 
